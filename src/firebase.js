ac07c4bd24c48fccabcffe10b5bc0610204b9e3b
Nishant <choudhary123nishant@gmail.com>
2021-08-29 22:55:23 +0530

    🚀 added environment variable

--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,20 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-import config from './config'
+console.log(process.env);
 
-// const config = {};
+const config = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+};
 
 firebase.initializeApp(config);
 const auth = firebase.auth();
 const db = firebase.firestore();
 
 export { auth, db };
-
-
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-// import { getAuth } from "firebase/auth";
-// import config from './config'
-
-// // const config = {};
-
-// const app = initializeApp(config);
-// export const auth = getAuth();
-// export const db = getFirestore();
-
-// export default app;
 
